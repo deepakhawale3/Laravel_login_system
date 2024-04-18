0a0db0ee0ad5b714bc5fc710718c40f6180fcc20
deepakhawale3 <deepakhawale3@gmail.com>
2024-04-18 19:13:40 +0530

    Changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="PQF_58Gwsw8YQNlMI6NT7cHGEc1tiFxDIcHxHWXAums" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
